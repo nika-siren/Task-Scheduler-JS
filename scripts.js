77d724773770d30506f59abaa282f50b437d21f7
nika-siren <rafandsulfus@gmail.com>
2024-09-26 17:59:35 +0400

    refactor(components): add UI_ELEMENTS, change tasks.length === 0 stroke, add ternary operator

--- scripts.js
+++ scripts.js
@@ -1,47 +1,47 @@
-const taskInput = document.getElementById("taskInput");
-const addTaskBtn = document.getElementById("addTaskBtn");
-const clearListBtn = document.getElementById("clearListBtn");
-const taskList = document.getElementById("taskList");
-const noTasksMessage = document.getElementById("noTasksMessage");
+const UI_ELEMENTS = {
+  taskInput: document.getElementById("taskInput"),
+  addTaskBtn: document.getElementById("addTaskBtn"),
+  clearListBtn: document.getElementById("clearListBtn"),
+  taskList: document.getElementById("taskList"),
+  noTasksMessage: document.getElementById("noTasksMessage"),
+};
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 function renderTasks() {
-  taskList.innerHTML = "";
-  clearListBtn.disabled = tasks.length === 0;
+  UI_ELEMENTS.taskList.innerHTML = "";
+  UI_ELEMENTS.clearListBtn.disabled = !tasks.length;
 
   tasks.forEach((task, index) => {
     const taskItem = document.createElement("div");
     taskItem.innerHTML = `<input type="checkbox" id="task${index}" />
                                   <label for="task${index}">${task}</label>`;
-    taskList.appendChild(taskItem);
+    UI_ELEMENTS.taskList.appendChild(taskItem);
 
     const checkbox = document.getElementById(`task${index}`);
     checkbox.addEventListener("change", () => {
-      if (checkbox.checked) {
-        taskItem.style.textDecoration = "line-through";
-      } else {
-        taskItem.style.textDecoration = "none";
-      }
+      taskItem.style.textDecoration = checkbox.checked
+        ? "line-through"
+        : "none";
     });
   });
 }
 
 renderTasks();
 
-addTaskBtn.addEventListener("click", () => {
-  const taskValue = taskInput.value.trim();
+UI_ELEMENTS.addTaskBtn.addEventListener("click", () => {
+  const taskValue = UI_ELEMENTS.taskInput.value.trim();
   if (taskValue) {
     tasks.push(taskValue);
     localStorage.setItem("tasks", JSON.stringify(tasks));
     renderTasks();
-    taskInput.value = "";
-    noTasksMessage.classList.add("hidden");
+    UI_ELEMENTS.taskInput.value = "";
+    UI_ELEMENTS.noTasksMessage.classList.add("hidden");
   }
 });
 
-clearListBtn.addEventListener("click", () => {
+UI_ELEMENTS.clearListBtn.addEventListener("click", () => {
   tasks = [];
   localStorage.setItem("tasks", JSON.stringify(tasks));
   renderTasks();
-  noTasksMessage.classList.remove("hidden");
+  UI_ELEMENTS.noTasksMessage.classList.remove("hidden");
 });
